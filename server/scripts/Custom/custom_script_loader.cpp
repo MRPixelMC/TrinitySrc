3705fdb2a131f96862dff2e33c4211a66ae171ea
MrPixel <mcmrpixel@gmail.com>
2017-02-09 13:11:11 +0100

    Reset

--- server/scripts/Custom/custom_script_loader.cpp
+++ server/scripts/Custom/custom_script_loader.cpp
@@ -16,13 +16,10 @@
  */
 
 // This is where scripts' loading functions should be declared:
-void AddSC_player_cooldowns();
-void AddSC_looter_pet();
 
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
 void AddCustomScripts()
 {
-	AddSC_player_cooldowns();
-	AddSC_looter_pet();
+
 }
